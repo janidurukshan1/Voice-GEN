1c3e0b73b1dcf0f5878e2649b565eebafc8754fa
janidurukshan1 <janidurukshan300@gmail.com>
2026-01-01 19:20:32 +0530

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE="ailk-invoice-v1";
+const ASSETS=[
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./ailk-logo.png"
+];
+
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch",e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
